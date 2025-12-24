99d50636628a5bf54fd45970ef8c08b91eb28459
GantangSatria <gantang610@gmail.com>
2025-12-24 19:03:39 +0700

    feat:general layout

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div className="justify-items-center">
+            <div className="flex items-center w-192 h-16 bg-white/75 rounded-full fixed top-8 border border-grey backdrop-blur-[2px]">
+            
+            </div>
+        </div>
+    )
+}
