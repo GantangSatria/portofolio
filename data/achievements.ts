c298af0afcfa8c69a2a059298e840aedda6c443a
GantangSatria <gantang610@gmail.com>
2026-01-07 15:36:46 +0700

    feat: updated data

--- data/achievements.ts
+++ data/achievements.ts
@@ -2,18 +2,18 @@ import type { Achievement } from "@/types/achievements"
 
 const achievements: Achievement[] = [
     {
-        title: "2nd-place",
-        venue: "",
-        year: "",
-        description: "",
-        image: ""
+        title: "2nd Place Software Development",
+        venue: "SEVENT 9.0 Universitas Telkom Purwokerto",
+        year: "2025",
+        description: "Developed 'SpeakBuddy', an AI-powered platform for children with speech delay. Engineered the backend using Go (Fiber) & MongoDB/PostgreSQL, and built the frontend with Next.js. Integrated Google Cloud Speech API and Gemini LLM for voice analysis.",
+        image: "/sevent.jpeg"
     },
     {
-        title: "best project collaboration",
-        venue: "",
-        year: "",
-        description: "",
-        image: ""
+        title: "Best Project Collaboration - Software Engineer Path",
+        venue: "Arkavidia Academya 9.0 Institut Teknologi Bandung",
+        year: "2025",
+        description: "Built 'MyCareer', a Flutter-based mobile app for informal workers. Implemented features like SkillQuest and JobPulse, and integrated AI-driven 'CareerBot' using Gemini API. Awarded for exceptional teamwork and agile execution.",
+        image: "/arkav.jpeg"
     }
 ]
 
--- data/experiences.ts
+++ data/experiences.ts
@@ -2,25 +2,25 @@ import type { Experience } from "@/types/experieces";
 
 const experiences: Experience[] = [
     {
-        title: "ASD",
-        venue: "",
-        year: "",
-        description: "",
-        image: ""
+        title: "Teaching Assistant of Data Structures and Algorithms",
+        venue: "Faculty of Computer Science, Universitas Brawijaya",
+        year: "Sep - Dec 2025",
+        description: "Mentored undergraduate students in understanding core concepts of data structures (Arrays, Linked Lists, Trees, Graphs) and algorithms (Sorting, Searching). Evaluated assignments and provided feedback on code complexity (Big O Notation) and efficiency using Java",
+        image: "/asprak-asd.jpeg"
     },
     {
-        title: "PAW",
-        venue: "",
-        year: "",
-        description: "",
-        image: ""
+        title: "Teaching Assistant of Web Application Programming",
+        venue: "Faculty of Computer Science, Universitas Brawijaya",
+        year: "Sep - Dec 2025",
+        description: "Guided students in developing dynamic web applications using MVC architecture. Assisted in debugging and implementing backend logic (Laravel/PHP) and frontend integration, ensuring best practices in database management and RESTful API implementation.",
+        image: "/asprak-paw.jpeg"
     },
         {
-        title: "Pemlan",
-        venue: "",
-        year: "",
-        description: "",
-        image: ""
+        title: "Teaching Assistant of Advance Programming",
+        venue: "Faculty of Computer Science, Universitas Brawijaya",
+        year: "Feb - June 2025",
+        description: "Facilitated interactive lab sessions to explore key Object-Oriented Programming (OOP) concepts such as inheritance, encapsulation, and polymorphism. Provided hands-on coding tasks to strengthen students' theoretical understanding.",
+        image: "/asprak-pemlan.jpeg"
     },
 ]
 
